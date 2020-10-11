5df356227b9ccd1898ddfe094ff235778dbc6023
Mason Adrales <masonadrales@Masons-MacBook-Pro-4.local>
2020-10-11 17:23:23 -0500

    Added title

--- mta49nFizzBuzz.py
+++ mta49nFizzBuzz.py
@@ -1,3 +1,5 @@
+print("FIZZBUZZ")
+print("This program is designed to print every number from 1-100. When the program encounters a multiple of 3, it will print Fizz. When it encounters a multiple of 5, it will print Buzz. When there is a multiple of both 3 and 5 it will print FizzBuzz.")
 for i in range(1, 101):
     if (i % 3 == 0 and i % 5 == 0):
         print("FizzBuzz")
